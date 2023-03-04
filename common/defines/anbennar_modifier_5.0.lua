5bc9fb7aadf42b44ab4fea0052c9d12d8f5eb80f
BERTUSVIDEOS <albertolivervalls@gmail.com>
2023-03-04 19:05:10 +0100

    bertus 2023-03-04 a

--- common/defines/anbennar_modifier_5.0.lua
+++ common/defines/anbennar_modifier_5.0.lua
@@ -136,6 +136,7 @@ NDefines.NMilitary.GALLEY_BONUS_COASTAL_SEA = 0.75	--0.5→0.75
 NDefines.NMilitary.MERCENARY_COMPANY_BUILDING_TIME = 0.50	--0.25→0.50
 NDefines.NMilitary.SIEGE_MEMORY = 19	--11→19
 NDefines.NMilitary.RAJPUT_BASE_COST_MODIFIER = 1.0  --5.0→1.0
+NDefines.NMilitary.LEADER_MAX_PIPS = 8 --6→8
 
 NDefines.NAI.MAX_CAV_PERCENTAGE = 20 --50→20 -- For modding actual ratio is dynamically computed but will be no higher than this.
 
