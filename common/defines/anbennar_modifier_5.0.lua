29f5a45913258d6aa183bb10f95cf9a5e7307fb0
BERTUSVIDEOS <albertolivervalls@gmail.com>
2024-10-13 18:21:56 +0200

    2024-10-13 a

--- common/defines/anbennar_modifier_5.0.lua
+++ common/defines/anbennar_modifier_5.0.lua
@@ -80,70 +80,71 @@ NDefines.NCountry.AUTONOMY_AT_CONQUEST_CLAIM = 20	                    -- 40→20
 NDefines.NCountry.REVOLUTION_EMBRACE_MIN_SPREAD = 0.50
 NDefines.NCountry.BREAK_ALLIANCE_ACTION_COOLDOWN = 0                    -- 10→0
 	
-NDefines.NEconomy.COLONIAL_MAINTENANCE_FACTOR = 2.0	                    -- 8→2          -- _EDEF_COLONIAL_MAINTENANCE_FACTOR_
+NDefines.NEconomy.COLONIAL_MAINTENANCE_FACTOR = 2.0	                    -- 8→2                  -- _EDEF_COLONIAL_MAINTENANCE_FACTOR_
 NDefines.NEconomy.TRADE_PROPAGATE_DIVIDER = 10	                        -- 5→10
-NDefines.NEconomy.CARAVAN_FACTOR = 20.0	                                -- 3→20         -- Development is divided by this factor do not set to zero!
-NDefines.NEconomy.MAX_BUILDING_SLOTS = 18	                            -- 12→18        -- Maximum number of buildings slots i.e. max buildings possible.
-NDefines.NEconomy.EXPELLED_MINORITY_DEV_BONUS_FACTOR = 0	            -- 0.2→0        -- Bonus development on minority expulsion completion multiplied by origin province development
-NDefines.NEconomy.EXPELLING_MINORITY_COLONIST_COST_FACTOR = 1.0	        -- 0.5→1.0      -- Colonist maintenance cost factor when expelling minority
-NDefines.NEconomy.EXPELLING_MINORITY_SETTLER_CHANCE_FACTOR = 0.005	    --0.001→-0.005  -- Settler chance bonus when expelling minority multiplied by origin province development
+NDefines.NEconomy.CARAVAN_FACTOR = 20.0	                                -- 3→20                 -- Development is divided by this factor do not set to zero!
+NDefines.NEconomy.MAX_BUILDING_SLOTS = 18	                            -- 12→18                -- Maximum number of buildings slots i.e. max buildings possible.
+NDefines.NEconomy.EXPELLED_MINORITY_DEV_BONUS_FACTOR = 0	            -- 0.2→0                -- Bonus development on minority expulsion completion multiplied by origin province development
+NDefines.NEconomy.EXPELLING_MINORITY_COLONIST_COST_FACTOR = 1.0	        -- 0.5→1.0              -- Colonist maintenance cost factor when expelling minority
+NDefines.NEconomy.EXPELLING_MINORITY_SETTLER_CHANCE_FACTOR = 0.005	    --0.001→-0.005          -- Settler chance bonus when expelling minority multiplied by origin province development
 NDefines.NEconomy.TRADE_POWER_HOME_BONUS = 0.05	                        --0.1→0.05
 NDefines.NEconomy.MERCHANT_MAX_POWER_BONUS = 5.0	                    --2→5
 NDefines.NEconomy.COLONY_MIN_AUTONOMY = 0	                            --50→0
 NDefines.NEconomy.LAND_TECH_MERC_MAINTENANCE_IMPACT = 0	                --0.08→-0
 
 NDefines.NMilitary.DAYS_PER_PHASE = 2                                   --3→2
-NDefines.NMilitary.ARMY_DRILL_YEARLY_GAIN = 5.0	                        --10→5      -- Gain from Drilling
+NDefines.NMilitary.ARMY_DRILL_YEARLY_GAIN = 5.0	                        --10→5                  -- Gain from Drilling
 NDefines.NMilitary.COSSACKS_SHOCK_DAMAGE_BONUS = 0	                    --0.1→0
 NDefines.NMilitary.STRELTSY_FIRE_DAMAGE_BONUS = 0	                    --0.1→0
 NDefines.NMilitary.BANNER_AGE_MULTIPLIER = 0.00	                        --0.5→0
-NDefines.NMilitary.BANNER_STARTING_STRENGTH = 0.001	                    --0.1→0.001 -- Starting strength of a banner regiment 1.0 being full strength
+NDefines.NMilitary.BANNER_STARTING_STRENGTH = 0.001	                    --0.1→0.001             -- Starting strength of a banner regiment 1.0 being full strength
 NDefines.NMilitary.CAWA_USES_CONSTRUCTION = 1	                        --0→1
 NDefines.NMilitary.CREATE_CAWA_MIL_COST = 0                             --2→0
 NDefines.NMilitary.CAWA_STARTING_STRENGTH = 1.0 				        -- 0.5→1.0
-NDefines.NMilitary.CAWA_STARTING_MORALE = 0.1	                        --0.5→0.1   -- Starting morale of a cawa regiment 1.0 being full strength
+NDefines.NMilitary.CAWA_STARTING_MORALE = 0.1	                        --0.5→0.1               -- Starting morale of a cawa regiment 1.0 being full strength
 NDefines.NMilitary.CAROLEAN_BASE_COST_MODIFIER = 1.0	                --1.0→1.0
 NDefines.NMilitary.CAROLEAN_STARTING_STRENGTH = 1.0                     --→1.0→0.1
 NDefines.NMilitary.HUSSARS_BASE_COST_MODIFIER = 3.0	                    --1.0→3.0
 NDefines.NMilitary.HUSSARS_MANPOWER_COST_MODIFIER = 0                   --1.0→0
 NDefines.NMilitary.HUSSARS_STARTING_STRENGTH = 0.01                     --1.0→0.01
 NDefines.NMilitary.HUSSARS_STARTING_MORALE = 0.01                       --0.1→0.01
-NDefines.NMilitary.FORT_DEVASTATION_IMPACT = -5	                        -- -10→-5   -- multiplied by fortlevel/max fortlevel in area per year.
-NDefines.NMilitary.CONTROL_DEVASTATION_IMPACT = -5	                    -- -1→-5    -- devastation recovery given by control
-NDefines.NMilitary.ARMY_ATTRITION_AT_SEA = 5	                        --10→5      -- How many percent attrition units take while loaded onto ships traveling open sea
-NDefines.NMilitary.GARRISON_SIZE = 2000                                 --1000→2000 -- GARRISON_SIZE
-NDefines.NMilitary.SIEGE_FORCE_NEEDED_MULTIPLIER = 0.5                  --3→0.5	    -- x times garrison to be able to siege.
-NDefines.NMilitary.SIEGE_GARRISON_SURRENDER = 200                       --100→200   -- A siege ends when there is less than 100 defenders left.
-NDefines.NMilitary.CAVALRY_SPEED = 1.0                                  --0.7→1.0   -- _MDEF_CAVALRY_SPEED = 10
-NDefines.NMilitary.CAVALRY_COST = 30.0                                  --25→30     -- _MDEF_CAVALRY_COST = 10
-NDefines.NMilitary.ARTILLERY_COST = 50.0                                --30→50     -- _MDEF_ARTILLERY_COST = 10
-NDefines.NMilitary.FORTRESS_COST = 0.25	                                --0.5→0.25  -- base fort cost
-NDefines.NMilitary.TRANSPORT_COST = 6	                                --12→6      -- _MDEF_TRANSPORT_COST = 10
-NDefines.NMilitary.TRANSPORT_TIME = 30                                  --365→30    -- _MDEF_TRANSPORT_TIME = 10
-NDefines.NMilitary.TRADITION_GAIN_LAND = 30	                            --20→30     -- Tradition gain base value from land combat.
-NDefines.NMilitary.LOOT_DEVASTATION_IMPACT = 20	                        --10→20     -- how much devastation full loot impacts
-NDefines.NMilitary.LOOTED_MAX = 20	                                    --5→20      -- Maximum amount of ducats (total) a province can be looted for each month.
-NDefines.NMilitary.BACK_LINE_MORALE_DAMAGE_TAKEN_MODIFIER = 0	        --0.4→0     -- Multiplier for morale damage taken by backrow
+NDefines.NMilitary.FORT_DEVASTATION_IMPACT = -5	                        -- -10→-5               -- multiplied by fortlevel/max fortlevel in area per year.
+NDefines.NMilitary.CONTROL_DEVASTATION_IMPACT = -5	                    -- -1→-5                -- devastation recovery given by control
+NDefines.NMilitary.ARMY_ATTRITION_AT_SEA = 5	                        --10→5                  -- How many percent attrition units take while loaded onto ships traveling open sea
+NDefines.NMilitary.GARRISON_SIZE = 2000                                 --1000→2000             -- GARRISON_SIZE
+NDefines.NMilitary.SIEGE_FORCE_NEEDED_MULTIPLIER = 0.5                  --3→0.5	                -- x times garrison to be able to siege.
+NDefines.NMilitary.SIEGE_GARRISON_SURRENDER = 200                       --100→200               -- A siege ends when there is less than 100 defenders left.
+NDefines.NMilitary.CAVALRY_SPEED = 1.0                                  --0.7→1.0               -- _MDEF_CAVALRY_SPEED = 10
+NDefines.NMilitary.CAVALRY_COST = 30.0                                  --25→30                 -- _MDEF_CAVALRY_COST = 10
+NDefines.NMilitary.ARTILLERY_COST = 50.0                                --30→50                 -- _MDEF_ARTILLERY_COST = 10
+NDefines.NMilitary.FORTRESS_COST = 0.25	                                --0.5→0.25              -- base fort cost
+NDefines.NMilitary.TRANSPORT_COST = 6	                                --12→6                  -- _MDEF_TRANSPORT_COST = 10
+NDefines.NMilitary.TRANSPORT_TIME = 30                                  --365→30                -- _MDEF_TRANSPORT_TIME = 10
+NDefines.NMilitary.TRADITION_GAIN_LAND = 30	                            --20→30                 -- Tradition gain base value from land combat.
+NDefines.NMilitary.LOOT_DEVASTATION_IMPACT = 20	                        --10→20                 -- how much devastation full loot impacts
+NDefines.NMilitary.LOOTED_MAX = 20	                                    --5→20                  -- Maximum amount of ducats (total) a province can be looted for each month.
+NDefines.NMilitary.BACK_LINE_MORALE_DAMAGE_TAKEN_MODIFIER = 0	        --0.4→0                 -- Multiplier for morale damage taken by backrow
 NDefines.NMilitary.BATTLE_DAILY_MORALE_DAMAGE = 0                       --0.01→0
 NDefines.NMilitary.BACK_LINE_REINFORCE_SPEED = 40                       --2→40
 NDefines.NMilitary.WARGOAL_MAX_BONUS = 40	                            --25→40
 NDefines.NMilitary.WARSCORE_MAX_FROM_BATTLES = 60	                    --40→60
 NDefines.NMilitary.DEFAULT_WARGOAL_TICKINGWARSCORE_BONUS = 1.111	    --0.4→1.111
-NDefines.NMilitary.INF_LOOT = 0.2	                                    --0.1→0.2   -- How many ducats/month will a single full strength infantry regiment loot?
-NDefines.NMilitary.CAV_LOOT = 1.0	                                    --0.3→1.0   -- How many ducats/month will a single full strength cavalry regiment loot?
+NDefines.NMilitary.INF_LOOT = 0.2	                                    --0.1→0.2               -- How many ducats/month will a single full strength infantry regiment loot?
+NDefines.NMilitary.CAV_LOOT = 1.0	                                    --0.3→1.0               -- How many ducats/month will a single full strength cavalry regiment loot?
 NDefines.NMilitary.NAVAL_CASUALTY_MIN_MORALE_DAMAGE = 0.0               -- 0.2→0.0
-NDefines.NMilitary.MERCENARY_COMPANY_HIRING_COST_MONTHS = 36	        --48→36     -- Initial hiring cost in months of maintenance
-NDefines.NMilitary.MERCENARY_COMPANY_MAX_REGIMENTS = 40	                --60→40     -- Maximum regiments
-NDefines.NMilitary.MERCENARY_COMPANY_MIN_REGIMENTS = 10	                --4→10      -- Maximum regiments
-NDefines.NMilitary.MERCENARY_COMPANY_MANPOWER_PER_REGIMENT = 1.00	    --2.0→1.00  -- Manpower reserve factor (multiplied by 1000 per regiment)
-NDefines.NMilitary.MERCENARY_COMPANY_HIRED_MANPOWER_RECOVERY_MOD = 2.0	-- How much to modify the length for manpower recovery if they are hired
-NDefines.NMilitary.NAVAL_BASE_DISENGAGEMENT_CHANCE = 0.2	            --0.1→0.2   -- Chance of disengaging from naval battles every day when out of morale 0-1 range
+NDefines.NMilitary.MERCENARY_COMPANY_HIRING_COST_MONTHS = 36	        --48→36                 -- Initial hiring cost in months of maintenance
+NDefines.NMilitary.MERCENARY_COMPANY_MAX_REGIMENTS = 40	                --60→40                 -- Maximum regiments
+NDefines.NMilitary.MERCENARY_COMPANY_MIN_REGIMENTS = 10	                --4→10                  -- Maximum regiments
+NDefines.NMilitary.MERCENARY_COMPANY_MANPOWER_PER_REGIMENT = 1.00	    --2.0→1.00              -- Manpower reserve factor (multiplied by 1000 per regiment)
+NDefines.NMilitary.MERCENARY_COMPANY_HIRED_MANPOWER_RECOVERY_MOD = 2.0	--                      -- How much to modify the length for manpower recovery if they are hired
+NDefines.NMilitary.NAVAL_BASE_DISENGAGEMENT_CHANCE = 0.20	            --0.10→0.20             -- Chance of disengaging from naval battles every day when out of morale 0-1 range
 NDefines.NMilitary.CANNOT_RETREAT_DAYS = 12
 NDefines.NMilitary.COMBAT_DICE_SIDE = 6	                                --10→6
 NDefines.NMilitary.ARMY_ATTRITION_AT_SEA = 1	                        --10→1
 NDefines.NMilitary.CREATE_HUSSARS_MIL_COST = 20                         --0→20
 NDefines.NMilitary.GALLEY_BONUS_INLAND_SEA = 1.5	                    --1.0→1.5
-NDefines.NMilitary.GALLEY_BONUS_COASTAL_SEA = 0.75	                    --0.5→0.75
+NDefines.NMilitary.GALLEY_BONUS_COASTAL_SEA = 0	                        --0.5→0
+NDefines.NMilitary.GALLEY_DICE_MALLUS_HIGH_SEA = -3				        --0→-3        Malus dice roll when not fighting in inland sea.
 NDefines.NMilitary.MERCENARY_COMPANY_BUILDING_TIME = 0.50	            --0.25→0.50
 NDefines.NMilitary.SIEGE_MEMORY = 19	                                --11→19
 NDefines.NMilitary.RAJPUT_BASE_COST_MODIFIER = 1.0                      --5.0→1.0
@@ -156,16 +157,16 @@ NDefines.NMilitary.STRAIT_CROSSING_PENALTY = 4                          --2→4
 NDefines.NMilitary.SEA_LANDING_PENALTY = 4                              --2→4
 
 
-NDefines.NAI.UNCONDITIONAL_SURRENDER_HOPELESS_STRENGTH_COMPARISON = 10      --20→10     -- if they have 20 times the army strength you do, wave the white flag
-NDefines.NAI.UNCONDITIONAL_SURRENDER_WARSCORE_LIMIT = -75                   -- AI won't surrender unless it's this bad.
-NDefines.NAI.PEACE_TIME_MONTHS = 12                                         -- 60→12    -- Tiempo de "AI stubbornness in a war", un modificador de aceptar la paz
-NDefines.NAI.PEACE_FORCE_BALANCE_FACTOR = 0                                 -- 0.2→0    -- Aceptacion de la IA al reconocer que el otro bando es mas fuerte
-NDefines.NAI.PEACE_WARGOAL_FACTOR = 0.5                                     -- 0→0.5    -- Aceptacion de la IA a rendirse por controlar el CB
-NDefines.NAI.PEACE_ALLY_WARSCORE_MULT = 1                                   -- 0.5→1    -- Entusiasmo por el warsocre global de la guerra
-NDefines.NAI.PEACE_ALLY_WARGOAL_MULT = 2                                    -- 0→2      -- Aceptacion de la IA aliada por no controlar el CB
-NDefines.NAI.MAX_CAV_PERCENTAGE = 20                                        --50→20 q   -- For modding actual ratio is dynamically computed but will be no higher than this.
+NDefines.NAI.UNCONDITIONAL_SURRENDER_HOPELESS_STRENGTH_COMPARISON = 10      --20→10             -- if they have 20 times the army strength you do, wave the white flag
+NDefines.NAI.UNCONDITIONAL_SURRENDER_WARSCORE_LIMIT = -75                   --                  -- AI won't surrender unless it's this bad.
+NDefines.NAI.PEACE_TIME_MONTHS = 12                                         -- 60→12            -- Tiempo de "AI stubbornness in a war", un modificador de aceptar la paz
+NDefines.NAI.PEACE_FORCE_BALANCE_FACTOR = 0                                 -- 0.2→0            -- Aceptacion de la IA al reconocer que el otro bando es mas fuerte
+NDefines.NAI.PEACE_WARGOAL_FACTOR = 0.5                                     -- 0→0.5            -- Aceptacion de la IA a rendirse por controlar el CB
+NDefines.NAI.PEACE_ALLY_WARSCORE_MULT = 1                                   -- 0.5→1            -- Entusiasmo por el warsocre global de la guerra
+NDefines.NAI.PEACE_ALLY_WARGOAL_MULT = 2                                    -- 0→2              -- Aceptacion de la IA aliada por no controlar el CB
+NDefines.NAI.MAX_CAV_PERCENTAGE = 20                                        --50→20 q           -- For modding actual ratio is dynamically computed but will be no higher than this.
 
-NDefines.NReligion.MAX_PAPAL_INFLUENCE = 300.0	                            --200→300   -- The maximum stored amount of papal influence for each country.
+NDefines.NReligion.MAX_PAPAL_INFLUENCE = 300.0	                            --200→300           -- The maximum stored amount of papal influence for each country.
 NDefines.NReligion.MAX_CHURCH_POWER = 400	                                --200→400
 
 NDefines.NGovernment.RUSSIAN_ABILITY_COST = 75	                            --100→75
